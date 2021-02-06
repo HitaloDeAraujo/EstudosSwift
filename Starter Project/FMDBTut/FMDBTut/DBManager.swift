1cacbcf8aa81da4cb07216bb74c2382ce743435e
admin <x253618@devops.mercantil.com.br>
2021-02-06 15:26:10 -0300

    Ïncluindo insercao de dados

--- Starter Project/FMDBTut/FMDBTut/DBManager.swift
+++ Starter Project/FMDBTut/FMDBTut/DBManager.swift
@@ -16,6 +16,16 @@ class DBManager: NSObject {
     var pathToDatabase: String!
     var database: FMDatabase!
     
+    
+    let field_MovieID = "movieID"
+    let field_MovieTitle = "title"
+    let field_MovieCategory = "category"
+    let field_MovieYear = "year"
+    let field_MovieURL = "movieURL"
+    let field_MovieCoverURL = "coverURL"
+    let field_MovieWatched = "watched"
+    let field_MovieLikes = "likes"
+    
     override init() {
         super.init()
         
@@ -33,19 +43,16 @@ class DBManager: NSObject {
             if database != nil {
                 if database.open(){
                     
-                    
                     let createMoviesTableQuery =
-                    "create table movies (\(field_MovieID) integer primary key autoincrement not null, \(field_MovieTitle) text not null, \(field_MovieCategory) text not null, \(field_MovieYear) interger not null, \(field_MovieURL) text, \(field_MovieCoverURL) text not null, \(field_MovieWatched) bool not null default 0, \(field_MovieLikes) integer not null)"
-                    
+                    "create table movies (\(field_MovieID) integer primary key autoincrement not null, \(field_MovieTitle) text not null, \(field_MovieCategory) text not null, \(field_MovieYear) integer not null, \(field_MovieURL) text, \(field_MovieCoverURL) text not null, \(field_MovieWatched) bool not null default 0, \(field_MovieLikes) integer not null)"
                   
                         do {
                             try database.executeUpdate(createMoviesTableQuery, values: nil)
                             created = true
                         }
                         catch {
-                        
+                            print(error.localizedDescription)
                         }
-                    
                 }
                 else {
                     print("Nao foi possivel abrir o banco de dados")
@@ -54,5 +61,58 @@ class DBManager: NSObject {
         }
         
         return created
+    }
+    
+    func openDatabase() -> Bool {
+        if database == nil {
+            if FileManager.default.fileExists(atPath: pathToDatabase) {
+                database = FMDatabase(path: pathToDatabase)
+            }
+        }
+        
+        if database != nil {
+            if database.open() {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func insertMovieData() {
+        if openDatabase() {
+            if let pathToMoviesFile = Bundle.main.path(forResource: "movies", ofType: "tsv"){
+                do{
+                    let moviesFileContents = try String(contentsOfFile: pathToMoviesFile)
+               
+                    let moviesData = moviesFileContents.components(separatedBy: "\r\n")
+                    
+                    var query = ""
+                    
+                    for movie in moviesData {
+                        let movieParts = movie.components(separatedBy: "\t")
+                        
+                        if movieParts.count == 5 {
+                            let movieTitle = movieParts[0]
+                            let movieCategory = movieParts[1]
+                            let movieYear = movieParts[2]
+                            let movieUrl = movieParts[3]
+                            let movieCoverURL = movieParts[4]
+                            
+                            query += "insert into movies (\(field_MovieID), \(field_MovieTitle), \(field_MovieCategory), \(field_MovieYear), \(field_MovieURL), \(field_MovieCoverURL), \(field_MovieWatched), \(field_MovieLikes)) values (null, '\(movieTitle)', '\(movieCategory)', '\(movieYear)', '\(movieUrl)', '\(movieCoverURL)', 0, 0);"
+                            
+                            if !database.executeStatements(query) {
+                                print("Falha ao inserir dados iniciais na base de dados")
+                                
+                                print(database.lastError(), database.lastErrorMessage())
+                            }
+                        }
+                    }
+                }
+                catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
