49c62195f1b29dd7e3d2d15d945840345d8fabd7
admin <x253816@devops.mercantil.com.br>
2021-02-20 17:53:46 -0300

    SwiftUI - Notificacoes

--- SwiftUINotificacoes/SwiftUINotificacoes/ContentView.swift
+++ SwiftUINotificacoes/SwiftUINotificacoes/ContentView.swift
@@ -9,8 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Enviar Notificação") {
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) {
+                success, error in
+                if success {
+                    print("autorizacao garantida")
+                } else if let error = error {
+                    print(error.localizedDescription)
+                }
+                
+                let content = UNMutableNotificationContent()
+                
+                content.title = "SwiftUI Notification App"
+                
+                content.subtitle = "enviada a partir do meu app"
+                
+                content.body = " Notificação Disparada"
+                
+                content.sound = UNNotificationSound.default
+                
+                let imageName = "applelogo"
+                
+                guard let imageURL = Bundle.main.url(forResource: imageName, withExtension: "png") else {
+                    return
+                }
+                
+                let attachment = try! UNNotificationAttachment(identifier: imageName, url: imageURL, options: .none)
+                
+                content.attachments = [attachment]
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: "notification.id.01", content: content, trigger: trigger)
+               
+                UNUserNotificationCenter.current().add(request)
+            }
+        }
     }
 }
 
@@ -19,3 +53,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
